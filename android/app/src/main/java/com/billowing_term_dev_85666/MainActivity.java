e35f7034a783ab6e102c1f2f0229ef5b6c535a35
Crowdbotics <engineering@crowdbotics.com>
2023-05-23 23:09:29 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/billowing_term_dev_85666/MainActivity.java
@@ -0,0 +1,15 @@
+package com.billowing_term_dev_85666;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "billowing_term_dev_85666";
+  }
+}
